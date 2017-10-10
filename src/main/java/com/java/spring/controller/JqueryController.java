2e783529f7ca4e405f350f2afbcf14a29a9a7fd8
GD <GD@DESKTOP-GO4SM0N>
2017-10-10 10:41:48 +0900

    171010

---
+++ src/main/java/com/java/spring/controller/JqueryController.java
@@ -0,0 +1,16 @@
+package com.java.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//스프링에서 사용하는 어노테이션 @Controller
+@Controller //특정 class에 주입하면 스프링이 알 수 있다
+public class JqueryController {
+	
+	//스프링에서 사용하는 어노테이션 @RequestMapping
+	@RequestMapping("/jquery") //특정 메소드에 주입하면 URL 정보를 매칭.
+	public String jquery(){
+		return "jQuery/index"; // view를 정의하는 부분
+	}
+
+}
