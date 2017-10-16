8889616c326304659df16d9caf54f22ffbd66e85
GD <GD@DESKTOP-GO4SM0N>
2017-10-16 09:55:46 +0900

    171013

--- src/main/webapp/resources/js/jquery2.js
+++ src/main/webapp/resources/js/jquery2.js
@@ -12,26 +12,26 @@
 				 $("input").val("");
 			 }); 
 			 
+			 
 			 $("#delete").click(function(){
-				 for(var j = 0; j<storage.length; j++){
-					 if($("input:checkbox").eq(j).prop("checked")){
-						storage.splice(j, 1);
-						set(storage);
-						createHtml();
+				 for(var j = 0; j<storage.length; j++){ 
+					 if($("input:checkbox").eq(j).prop("checked")){ //input 배열에서 checked가 된 경우만 
+						storage.splice(j, 1); //삭제하고
+						set(storage);  //로컬에 반영해서
+						createHtml();  //HTML에 적용
 						break;
 					 }
 				 }
 			 });
 			 
+			 
 			 $("#update").click(function(){
 				 for(var j = 0; j<storage.length; j++){
 					 if($("input:checkbox").eq(j).prop("checked")){
 						var text = $("input:text").val();
-						var newData = storage[j];
-						newData.text = text;
-						storage[j] = newData;
-						set(storage); 
-						createHtml();
+						storage[j].text = text;  //storage의 text라는 key의 value를 위의 input창에 입력된 text로 변경하고
+						set(storage);  //로컬스토리지 넣어서
+						createHtml(); //반영한다
 						$("input:text").val("");
 						break;
 					 }
@@ -57,30 +57,31 @@
 
 			if(storage.length > 0){
 				i = storage.length + 1;
-				
 				$("input:checkbox").click(function(){ 
-					 if($(this).prop("checked")){ 
+					if($(this).prop("checked")){ 
 						 $("input:checkbox").prop("checked",false); 
 						 $(this).prop("checked",true); 
+						 
 					 }else{
 						 $("input:checkbox").prop("checked",false); 
 					 }
 				 });
-				
 			}
 			
-				
-				
-							/************************************************************************
-							 * 1. 선택자 중에 input의 type은 여러가지가 올 수 있기 때문에 input:type를 선택할 수 있다
-							 * 예) <input type="text">   -> $("input:text")
-							 * 
-							 * 2. 체크박스는 결과값이 true & false를 리턴하기 때문에 jQuery에서 .prop()을 사용한다
-							 * 예) $("input:checkbox").prop('checked') <-- 결과값 가져오는 방법
-							 *
-							 * 3. 하위 태그를 찾기 위한 방법으로 find()를 이용하여 정보를 가져올 수 있다.
-							 * 예) $("h1").find("span")  <-- h1 태그 안에 span 태그 정보 가져오는 방법
-							 ************************************************************************/
-			 
 		 }
+		 
+
+			
+			
+			/************************************************************************
+			 * 1. 선택자 중에 input의 type은 여러가지가 올 수 있기 때문에 input:type를 선택할 수 있다
+			 * 예) <input type="text">   -> $("input:text")
+			 * 
+			 * 2. 체크박스는 결과값이 true & false를 리턴하기 때문에 jQuery에서 .prop()을 사용한다
+			 * 예) $("input:checkbox").prop('checked') <-- 결과값 가져오는 방법
+			 *
+			 * 3. 하위 태그를 찾기 위한 방법으로 find()를 이용하여 정보를 가져올 수 있다.
+			 * 예) $("h1").find("span")  <-- h1 태그 안에 span 태그 정보 가져오는 방법
+			 ************************************************************************/
+
 
